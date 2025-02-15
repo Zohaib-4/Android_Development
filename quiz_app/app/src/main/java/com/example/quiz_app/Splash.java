c6fc9148656d68af810d99a946bbffe898484614
Zohaib <sameer.zohaib363@gmail.com>
2025-02-15 16:32:53 +0500

    Update layout and add EditText background
    - Create `edittext_bg.xml` for custom EditText background.
    - Update `activity_main.xml` to apply the new background to the EditText.
    - Set hint for name.
    - Add bold text style to the enter name TextView.
    - Adjust button height and text size.

---
+++ quiz_app/app/src/main/java/com/example/quiz_app/Splash.java
@@ -0,0 +1,18 @@
+package com.example.quiz_app;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+   Animation fading_splash;
+
+   protected void onCreate
+}
+
