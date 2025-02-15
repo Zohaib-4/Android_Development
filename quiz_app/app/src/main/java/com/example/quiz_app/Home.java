9a09cbab94a5a5428aa3d4968cda11a1c39631b3
Zohaib <sameer.zohaib363@gmail.com>
2025-02-15 16:49:43 +0500

    Implement splash screen and home activities. Added the splash screen activity with a fading animation and created a home activity as the main screen.

--- quiz_app/app/src/main/java/com/example/quiz_app/Home.java
+++ quiz_app/app/src/main/java/com/example/quiz_app/Home.java
@@ -1,9 +1,6 @@
 package com.example.quiz_app;
 
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Splash extends AppCompatActivity {
-
-   Animation fading_splash;
-   ImageView spImage;
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        spImage = findViewById(R.id.spImage);
-        fading_splash = AnimationUtils.loadAnimation(this, R.anim.fading_splash);
-
-        spImage.startAnimation(fading_splash);
     }
 }
-
