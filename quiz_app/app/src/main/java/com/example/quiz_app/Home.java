152757c2f3ba629d693c5cab72a8fa84c7dabf39
Zohaib <sameer.zohaib363@gmail.com>
2025-02-15 20:40:37 +0500

    Splash screen implementation with animation.

--- quiz_app/app/src/main/java/com/example/quiz_app/Home.java
+++ quiz_app/app/src/main/java/com/example/quiz_app/Home.java
@@ -1,7 +1,11 @@
 package com.example.quiz_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
+    EditText etName;
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,30 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etName = findViewById(R.id.etName);
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String person_name = etName.getText().toString().trim();
+
+                nextActivity(person_name);
+            }
+        });
     }
 
-    public void btnStartClick(View view) {
+    private void nextActivity(String person_name) {
+
+        if (person_name.isEmpty()) {
+            etName.setError("Please enter your name");
+            return;
+        }
+
+        Intent intent = new Intent(Home.this, MainActivity.class);
+        intent.putExtra("person_name", person_name);
+        startActivity(intent);
     }
+
 }
--- quiz_app/app/src/main/java/com/example/quiz_app/quiz1.java
+++ quiz_app/app/src/main/java/com/example/quiz_app/quiz1.java
@@ -1,12 +1,6 @@
 package com.example.quiz_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Animation fading_splash;
-    ImageView spImage;
+public class quiz1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        spImage = findViewById(R.id.spImage);
-        fading_splash = AnimationUtils.loadAnimation(this, R.anim.fading_splash);
-
-        spImage.startAnimation(fading_splash);
-
-        new Handler()
-                .postDelayed(()->{
-                    startActivity(new Intent(MainActivity.this, Home.class));
-                    finish();
-                }, 8000);
     }
 }
