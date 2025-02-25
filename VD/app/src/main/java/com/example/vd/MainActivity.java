3427f3773a585ffc41da1c20fd15f37623a23ba3
Zohaib <sameer.zohaib363@gmail.com>
2025-02-25 21:50:48 +0500

    Add `android:id="@+id/tvView"` to TextView in activity_main.xml and update corresponding `findViewById` in MainActivity.java.

--- VD/app/src/main/java/com/example/vd/MainActivity.java
+++ VD/app/src/main/java/com/example/vd/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        TextView textView = findViewById(R.id.text);
+        TextView textView = findViewById(R.id.tvView);
     }
 }
