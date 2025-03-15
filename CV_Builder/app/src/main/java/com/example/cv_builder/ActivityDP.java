883dd12874d657c7aa0445a884a801a63170becd
Zohaib <sameer.zohaib363@gmail.com>
2025-03-15 22:10:00 +0500

    Add image selection button and ImageView to the display picture activity layout.

--- CV_Builder/app/src/main/java/com/example/cv_builder/ActivityDP.java
+++ CV_Builder/app/src/main/java/com/example/cv_builder/ActivityDP.java
@@ -1,6 +1,8 @@
 package com.example.cv_builder;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class ActivityDP extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView ivProfilePicture = findViewById(R.id.ivProfilePicture);
+        Button btnSelectImage = findViewById(R.id.btnSelectImage);
+
+        btnSelectImage.setOnClickListener(v -> {
+
+
+        });
+
     }
 }
